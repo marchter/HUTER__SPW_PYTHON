35d790cef9a0174823797138b1f680c427354c95
marchter <62132456+marchter@users.noreply.github.com>
2022-09-27 22:07:49 +0200

    Create Huter_Lotto.py
    Simple Lotto and statistic.

---
+++ Aufgabe01/Huter_Lotto.py
@@ -0,0 +1,27 @@
+import random
+import numpy as np
+
+#6 zufallszahlen
+def lotto():
+    i = 0
+    a = np.arange(1,46)
+    while i < 6:
+        zz = random.randint(a[0], a[44-i])
+        a[zz-1], a[44 - i] = a[44 - i], a[zz-1]
+        i = i + 1
+
+        return zz
+
+
+#statistik mit dicts
+def dict_lotto():
+    dict = {}
+    for i in range(1,46):
+        dict[i]=0
+    for i in range(0,1000):
+        dict[int(random.random()*45+1)]+=1
+
+    return dict
+
+
+print("Lottoziehung: " + str(lotto()) + "\nStatistik: "+ str(dict_lotto()))
