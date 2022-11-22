8e5125166046de3f065a1acd1f1f1400dba6cb0b
marce <marce@DESKTOP-JGF0N5H>
2022-11-22 22:23:00 +0100

    getInput

--- Aufgabe03/rpssl.py
+++ Aufgabe03/rpssl.py
@@ -1,11 +1,15 @@
 import random
 
+symbols = ["Rock", "Paper", "Scissors", "Spock", "Lizard"]
 
 
-
+def getInput():
+    user_input = input("Choose between 0 and 4 \n ")
+    return int(user_input) if user_input.isdigit() else "false input"
 
 
 def main():
     pass
+
 if __name__ == "__main__":
     main()
