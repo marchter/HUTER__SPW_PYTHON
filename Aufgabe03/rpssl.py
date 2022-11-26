c593955ccb89e0288a94401901d2c99d194777b3
marce <marce@DESKTOP-JGF0N5H>
2022-11-26 16:12:43 +0100

    pvp und pvc

--- Aufgabe03/rpssl.py
+++ Aufgabe03/rpssl.py
@@ -7,16 +7,35 @@ def getInput():
     for i in range(len(symbols)):
         print(str(i) + " equals " + symbols[i])
     user_input = input("Choose between 0 and 4 \n ")
-    return int(user_input) if user_input.isdigit() and int(user_input) in range(len(symbols )) else "false input"
+    return int(user_input) if user_input.isdigit() and int(user_input) in range(len(symbols)) else "false input"
 
-def getLoser(p1, p2):
-    #überlegen mit wie sie verzweigt sind, also 1 mit die nächsten 2
-    return p1 if (p1 - (p2 % 5)) in range(0,2) else p2
+
+def getWinner(p1, p2):
+    # überlegen mit wie sie verzweigt sind, also 1 mit die nächsten 2
+    # 5 bedeutet dass niemand gewinnt, sonst wird der gewinner zurückgegeben.
+
+    if p1 == p2: return 5
+    if (p1 + 1) % 4 == p2: return p2
+    if (p1 + 3) % 4 == 2: return p2
+    return p1
+
+def pvp():
+    print("The Player that chose "+symbols[getWinner(getInput(),getInput())]+" won!" )
+
+def pvc():
+    #mit print zwar schönere ausgabe aber für datenspeicherung nervig. :D
+    #print("The Player that chose "+symbols[getWinner(getInput(),random.randint(0,4))]+" won!" )
+    return getWinner(getInput(),random.randint(0,len(symbols)))
 
 
 def main():
-    #print(getInput())
-    print(getLoser(0,1))
+    # print(getInput())
+    #print(getWinner(0,0))
+    #print(getWinner(getInput(), getInput()))
+    #pvp()
+    print(pvc())
+
+
 
 if __name__ == "__main__":
     main()
