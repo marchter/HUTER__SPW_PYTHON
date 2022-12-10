e00bb823070cee842251c41badb375898ae9e8c8
marce <marce@DESKTOP-JGF0N5H>
2022-12-10 18:49:10 +0100

    menü, statistic

--- Aufgabe03/rpssl.py
+++ Aufgabe03/rpssl.py
@@ -13,8 +13,8 @@ def getInput():
 def getWinner(p1, p2):
     # überlegen mit wie sie verzweigt sind, also 1 mit die nächsten 2
     # 5 bedeutet dass niemand gewinnt, sonst wird der gewinner zurückgegeben.
-    check_value = (p1 - p2) % 5
-    if p1 == p2: return 5
+    check_value = (p1 - p2) % len(symbols)
+    if p1 == p2: return -1
     if check_value == 1 or check_value == 2: return p1
     if check_value == 3 or check_value == 4: return p2
 
@@ -23,17 +23,17 @@ def getWinner(p1, p2):
 def pvp():
     print("The Player that chose " + symbols[getWinner(getInput(), getInput())] + " won!")
 
-
 def pvc():
     # mit print zwar schönere ausgabe aber für datenspeicherung nervig. :D
-    # print("The Player that chose "+symbols[getWinner(getInput(),random.randint(0,4))]+" won!" )
-    return getWinner(getInput(), random.randint(0, len(symbols)-1))
-
-
-def pvc_write():
-    file1 = open("pvc_count.txt", "a")
+    #print("The Player that chose "+symbols[getWinner(getInput(),random.randint(0,4))]+" won!" )
     p = getInput()
     c = random.randint(0, len(symbols)-1)
+    pvc_write(p,c)
+    return getWinner(p,c)
+
+
+def pvc_write(p,c):
+    file1 = open("pvc_count.txt", "a")
     if getWinner(p,c)== -1: file1.write("\ndraw "+ str(p) + str(c))
     elif p == getWinner(p, c):
         file1.write("\np won with " + str(p) + "\ncom lost with "+str(c))
@@ -43,15 +43,26 @@ def pvc_write():
 
 
 def pvc_count():
-    pvc_write()
     file1 = open("pvc_count.txt", "r")
     data = str(file1.read())
-    return ("\np1 won: " + str(data.count("p1")) + " \ncom won: " + str(data.count("com")))
+    return ("\np1 won: " + str(data.count("p won")) + " \ncom won: " + str(data.count("com won")))
 
 def symbols_count():
     file1 = open("pvc_count.txt", "r")
     data = str(file1.read())
     return "\nSymbols used:\n Rock: " + str(data.count("0")) + "\n Paper: " + str(data.count("1")) + "\n Scissors: " + str(data.count("2")) + "\n Spock: " + str(data.count("3"))+ "\n Lizard: " + str(data.count("4"))
+
+def statistic():
+    print(symbols_count()+"\n\n Winners:" + pvc_count())
+def upload():
+    pass
+
+def menü():
+    print("What do you want to play?\nWe offer:\n [0] pvp\n [1] pvc\n [2] statistic\n [3] upload data")
+    methods = [pvp, pvc, statistic, upload]
+    inp = input("Choose a option")
+    methods[int(inp)]()
+
 
 
 def main():
@@ -61,8 +72,9 @@ def main():
     # pvp()
     # print(pvc())
     #print(pvc_count())
-    pvc_count()
-    print(symbols_count())
+    #pvc_count()
+    #print(symbols_count())
+    menü()
 
 if __name__ == "__main__":
     main()
